23e744fa491231afe30cecf8973bff001ecd51f2
julienhocq <julienhocq@gmail.com>
2022-06-06 19:29:32 -0400

    basic itemdetails page

--- client/src/components/ItemDetails.js
+++ client/src/components/ItemDetails.js
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import styled from "styled-components";
 
 
 const ItemDetails = () => {
+    const [item, setItem] = useState([]);
+
+    const {itemById} = useParams()
+
+
+  // fetching product info by itemId
+useEffect(() =>{
+    const fetchProduct = async () => {
+        const data = await fetch (`/api/item/${itemById}`)
+        const json = await data.json()
+        setItem(json.data);
+
+    } 
+    fetchProduct()
+}, [itemById])
+
+console.log('', item);
+
 
     return (
-        <div>Youpi</div>
+<>
+        <Img src={item.image} />
+        <p>{item.name}</p>
+        <p>{item.description}</p>
+        <p>$ {item.priceDaily}</p>
+
+        </>
     )
 }
+
+const Img = styled.img`
+max-width: 600px;
+`
 
 export default ItemDetails
