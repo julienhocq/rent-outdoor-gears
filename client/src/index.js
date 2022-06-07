76eab0249f6a2d38180a598ff9f359875bf588c5
julienhocq <julienhocq@gmail.com>
2022-06-06 20:16:22 -0400

    add Item provider to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { ItemProvider } from "./components/context/Context";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <ItemProvider>
       <App />
+      </ItemProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
