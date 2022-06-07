14c30d9e06df6d7f06eea0bd50d9bf2996a3f8a8
julienhocq <julienhocq@gmail.com>
2022-06-07 14:07:06 -0400

    add owner context

--- client/src/components/context/Context.js
+++ client/src/components/context/Context.js
@@ -1,33 +1,57 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
+export const ItemContext = createContext();
 
-export const ItemContext = createContext()
+export const ItemProvider = ({ children }) => {
+//   const [items, setItems] = useState();
+//   console.log("erer ", items);
 
-export const ItemProvider = ({children}) => {
-    const [items, setItems] = useState();
-    console.log('erer ', items);
+  const [selectedItem, setSelectedItem] = useState();
+  console.log("selected in context", selectedItem);
 
-    const [selectedItem, setSelectedItem] = useState(null);
-    console.log('selected in context', selectedItem);
-    
-
-    return (
-        <ItemContext.Provider
-        value={{
-            items,
-            setItems,
-            selectedItem,
-            setSelectedItem,
-        }}
-        >
-
-        {children}
-
-        </ItemContext.Provider>
-    )
-
-}
+  return (
+    <ItemContext.Provider
+      value={{
+        // items,
+        // setItems,
+        selectedItem,
+        setSelectedItem,
+      }}
+    >
+      {children}
+    </ItemContext.Provider>
+  );
+};
 
 // export const ItemState = () => {
 //     return useContext(ItemContext)
 // }
+
+//context to pass user logged-in status across App.
+export const OwnerContext = createContext(null);
+
+
+export const OwnerProvider = ({ children }) => {
+  const [owner, setOwner] = usePersistedState("owner", null);
+
+  return (
+    <OwnerContext.Provider value={{ owner, setOwner }}>
+      {children}
+    </OwnerContext.Provider>
+  );
+};
+
+//custom hook for keeping track of the user that is logged in
+const usePersistedState = (localStorageName, initialValue) => {
+  const [state, setState] = useState(() => {
+    const storedValue = window.localStorage.getItem(localStorageName);
+
+    return storedValue !== null ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(localStorageName, JSON.stringify(state));
+  }, [localStorageName, state]);
+
+  return [state, setState];
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { ItemProvider } from "./components/context/Context";
-
+import { ItemProvider, OwnerProvider } from "./components/context/Context";
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <OwnerProvider>
     <ItemProvider>
       <App />
       </ItemProvider>
+      </OwnerProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
