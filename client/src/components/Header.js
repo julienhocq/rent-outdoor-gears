c4329820e554eaeb6c56e7057c082703e668fa40
julienhocq <julienhocq@gmail.com>
2022-06-07 14:05:41 -0400

    add login & log out

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,49 @@
 import styled from "styled-components";
+import {GiExitDoor} from "react-icons/gi"
 import { FaUserLock } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { NavLink, Link, useLocation } from "react-router-dom";
+import { OwnerContext } from "./context/Context";
+import { useContext } from "react";
+
+
 
 const Header = () => {
+  const { owner, setOwner } = useContext(OwnerContext);
+
+  const handleLogout = () => {
+    setOwner(null);
+  };
+
   return (
     <HeaderSection>
-        <Link to="/">
-      <LogoHeader>Rent Adventure</LogoHeader>
-      </Link>
+      <NavLinkStyled to="/">
+        <LogoHeader>Rent Adventure</LogoHeader>
+      </NavLinkStyled>
+      <NavLinkStyled
+        // if user is not logged in, then this link directs to login page. If user is logged in, then clicking here opens a drop down menu
+        to={!owner && "/login"}
+        // onClick={() => setToggleUserMenu(!toggleUserMenu)}
+        // ref={userMenuRef}
+      >
+        {!owner ? (
+        <>
+           <FaUserLock size={30} color={"blue"} />
+          <LoginDesign>LOGIN</LoginDesign>
+          </>
+        ) : (
+          <>
+          <Greeting>Hi,{owner}</Greeting>
+          <LogOutWrapper onClick={handleLogout}>
+          <GiExitDoor size={30} color={"blue"} />
+          <NavLinkStyled to="/">
+          <LoginDesign>LOGOUT</LoginDesign>
+          </NavLinkStyled>
+          </LogOutWrapper>
+          </>
+        )}
+        
 
-      <FaUserLock size={30} color={"blue"} />
+      </NavLinkStyled>
     </HeaderSection>
   );
 };
@@ -21,7 +55,7 @@ const HeaderSection = styled.header`
   margin-bottom: 20px;
   margin-top: 0px !important;
   color: white;
-  background-color: #AEEAFF;
+  background-color: #aeeaff;
   padding: 20px 20px;
 `;
 
@@ -31,5 +65,30 @@ const LogoHeader = styled.h1`
   font-size: 2.7em;
   color: black;
 `;
+
+const NavLinkStyled = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  gap: 10px;
+`;
+
+const Greeting = styled.div`
+  font-weight: bold;
+  text-decoration: none;
+  color: blue;
+`;
+
+const LoginDesign = styled.span`
+  color: blue;
+  font-size: 1.2em;
+  font-weight: 700;
+`;
+
+const Logout = styled.div`
+`
+
+const LogOutWrapper = styled.div`
+`
 
 export default Header;
