71fc4b25ecc5621c02da9dc3d5ee3d75cd804fed
julienhocq <julienhocq@gmail.com>
2022-06-07 16:28:44 -0400

    fetch owners data

--- client/src/components/OwnerHome.js
+++ client/src/components/OwnerHome.js
@@ -1,12 +1,23 @@
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const OwnerHome = () => {
+  const [owner, setOwner] = useState([]);
+  const { profileById } = useParams();
 
-    return (
- <div>Hey</div>
-)
+  useEffect(() => {
+    const fetchOwner = async () => {
+      const data = await fetch(`/api/profile/${profileById}`);
+      const json = await data.json();
+      setOwner(json.data);
+    };
+    fetchOwner();
+  }, [profileById]);
 
-}
+  console.log("owner is", owner);
 
-export default OwnerHome
+  return <div>Hey</div>;
+};
 
+export default OwnerHome;
