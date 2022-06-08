74a762cdd5eb6976a57d675f0838ad07c21577af
julienhocq <julienhocq@gmail.com>
2022-06-07 23:53:37 -0400

    post new Item handler (without image)

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const multer  = require('multer')
+// const upload = multer({ dest: 'uploads/' })
 
 const { getAllItems } = require("./handlers/getAllItems");
 const { getItemsByCategory } = require("./handlers/ItemsByCategory");
 const { getItemById } = require("./handlers/singleItem");
 const { register } = require("./handlers/register");
 const {getOwnerById} = require("./handlers/singleOwner")
+const {postItem} = require("./handlers/postItem")
+
+const cloudinary = require('./middlewares/cloudinary')
+const fs = require('fs')
+const upload = require('./middlewares/multer')
+// const {postImages} = require('./handlers/postImages')
 
 const { login } = require("./handlers/login");
 
@@ -37,6 +45,33 @@ express()
   .post("/api/owner/register", register)
   // user - login
   .post("/api/owner/login", login)
+
+  .post("/api/item", postItem)
+
+  // .use("/upload-images", upload.array("image"), async (req, res) => {
+
+  //   const uploader = async (path) => await cloudinary.uploads(path, 'Images')
+
+  //   if(req.method === 'POST') {
+  //     const urls = []
+  //     const files = req.files;
+  //       for(const file of files) {
+  //           const {path} = file;
+  //           const newPath = await uploader(path)
+  //           urls.push(newPath)
+  //           fs.unlinkSync(path)
+  //       }
+     
+  //   res.status(200).json({
+  //       message: 'Success! images are uploaded',
+  //       data: urls
+  //   })
+  //   } else {
+  //       res.status(400).json({
+  //           err: `${req.method} method not allowed`
+  //       })
+  //   }
+  // })
 
   // this is our catch all endpoint.
   .get("*", (req, res) => {
