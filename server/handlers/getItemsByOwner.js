cc495444b21e687e6953799aab144a294b7e595f
julienhocq <julienhocq@gmail.com>
2022-06-11 12:27:22 -0400

    add handler get all items of a specific user

--- server/handlers/getItemsByOwner.js
+++ server/handlers/getItemsByOwner.js
@@ -7,10 +7,39 @@ const options = {
   useUnifiedTopology: true,
 };
 
-
 const getItemsByOwner = async (req, res) => {
+  const _id = req.params.profileById;
+  console.log("id is", _id);
+  const client = new MongoClient(MONGO_URI, options);
 
+  try {
+    await client.connect();
+    console.log("Connected!");
 
-}
+    const db = client.db("RentOutdoorGears");
 
-module.exports = {getItemsByOwner}
+    const findOwner = await db.collection("owners").findOne({ _id });
+    console.log("findOwner", findOwner);
+    console.log("findOwner._id", findOwner._id);
+
+    const allProducts = await db.collection("products").find().toArray();
+    const findOwnerItems = allProducts.filter((e) => {
+      return e.OwnerId === findOwner._id;
+    });
+
+    console.log("findOwnerItems", findOwnerItems);
+
+    return res.status(200).json({
+      status: 200,
+      data: findOwnerItems,
+      message: "This is the item(s) of this user",
+    });
+  } catch (err) {
+    res.status(500).json({ status: 500, message: err.message });
+  } finally {
+    client.close();
+    console.log("disconnected!");
+  }
+};
+
+module.exports = { getItemsByOwner };
