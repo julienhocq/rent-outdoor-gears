2cc8cf9c9e31c25e20baaf77c108006d6d952e45
julienhocq <julienhocq@gmail.com>
2022-06-11 00:23:41 -0400

    add feth all items (for 1 user)

--- client/src/components/OwnerHome.js
+++ client/src/components/OwnerHome.js
@@ -4,35 +4,57 @@ import styled from "styled-components";
 
 const OwnerHome = () => {
   const [owner, setOwner] = useState([]);
+  const [items, setItems] = useState([]);
   const { profileById } = useParams();
 
   useEffect(() => {
     const fetchOwner = async () => {
-      const data = await fetch(`/api/profile/${profileById}`);
-      const json = await data.json();
-      setOwner(json.data);
+      await fetch(`/api/profile/${profileById}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setOwner(data.data);
+        });
     };
     fetchOwner();
   }, [profileById]);
 
+  useEffect(() => {
+    const fetchItemsOwner = async () => {
+      await fetch(`/api/profile/${profileById}/items`)
+        .then((res) => res.json())
+        .then((data) => {
+          setItems(data.data);
+        });
+    };
+    fetchItemsOwner();
+  }, [profileById]);
+
   console.log("owner is", owner);
+  console.log("items is", items);
+  console.log("items 1", items[0]);
+  console.log("items 2", items[1]);
 
   return (
-<>
-    <Img src={owner.image} />
-    <p>{owner.username}</p>
-<p>Your items: </p>
+    <>
+      {owner && items && (
+        <>
+          <Img src={owner.image} />
+          <p>{owner.username}</p>
+          <p>Your items: </p>
 
-<p>Add new item</p>
+          {items.map((item) => (
+            <p>{item.name}</p>
+          ))}
 
+          <p>Add new item</p>
+        </>
+      )}
     </>
-
-  )
+  );
 };
 
 const Img = styled.img`
-max-width: 80px;
-`
-
+  max-width: 80px;
+`;
 
 export default OwnerHome;
