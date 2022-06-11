c407af00ff37f02fb298475016360023bd50f6a8
julienhocq <julienhocq@gmail.com>
2022-06-11 18:45:01 -0400

    finalise book item (BE) - without validations yet

--- server/handlers/getAllReservationByItemId.js
+++ server/handlers/getAllReservationByItemId.js
@@ -7,27 +7,34 @@ const options = {
   useUnifiedTopology: true,
 };
 
-const getAllReservationByItemId = async (req, res) =>{
+const getAllReservationByItemId = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const _id = req.params.itemById;
 
-    const client = new MongoClient(MONGO_URI, options);
+  console.log("whasssss");
+  try {
+    await client.connect();
+    console.log("Connected!");
 
+    const db = client.db("RentOutdoorGears");
 
-console.log('whasssss');
-    try {
-        await client.connect();
-        console.log("Connected!");
-    
-        const db = client.db("RentOutdoorGears");
-    
+    const allReservations = await db.collection("reservations").find().toArray();
+    const findReservations = allReservations.filter((e) => {
+      return e.itemId === _id;
+    });
+    console.log("findReservations", findReservations);
 
-    }
-    catch (err) {
-        res.status(500).json({ status: 500, message: err.message });
-      } finally {
-        client.close();
-        console.log("disconnected!");
-      }
+    return res.status(200).json({
+      status: 200,
+      data: findReservations,
+      message: "This is all reservations for this item",
+    });
+  } catch (err) {
+    res.status(500).json({ status: 500, message: err.message });
+  } finally {
+    client.close();
+    console.log("disconnected!");
+  }
+};
 
-}
-
-module.exports = {getAllReservationByItemId}
+module.exports = { getAllReservationByItemId };
