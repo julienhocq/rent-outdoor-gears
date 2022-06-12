f0db30aa1f92455174cf5659d5c5fdbff928f39a
julienhocq <julienhocq@gmail.com>
2022-06-12 17:45:30 -0400

    handle can't book if not a user

--- client/src/components/CalendarReservation.js
+++ client/src/components/CalendarReservation.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import styled from "styled-components";
+import { OwnerContext } from "./context/Context";
 
 import { isWithinInterval } from "date-fns";
 import ItemDetails from "./ItemDetails";
@@ -11,13 +12,87 @@ import ItemDetails from "./ItemDetails";
 const CalendarReservation = ({ owner, item }) => {
   const { itemById } = useParams();
 
-  const [date, setDate] = useState();
-  // console.log('new Date', new Date());
+  const { owner : user } = useContext(OwnerContext)
+  console.log('user', user);
 
+
+  const [date, setDate] = useState(new Date());
+  const [resa, setResa] = useState([])
+  const [essai, setEssai] = useState()
+
+  // console.log('new Date', new Date());
+  // const [disabledRanges, setDisabledRanges] = useState([])
+
+  // const disabledRanges = [
+  //   [new Date(2022, 5, 25), new Date(2022, 5, 28)],
+  //   [new Date(2022, 5, 17), new Date(2022, 5, 22)],
+  // ];
   const disabledRanges = [
-    [new Date(2022, 5, 25), new Date(2022, 5, 28)],
-    [new Date(2022, 5, 17), new Date(2022, 5, 22)],
+    // [new Date(2022, 5, 25), new Date(2022, 5, 25)],
+    //  ["2022-07-13", "2022-07-15"]
+    
   ];
+
+
+console.log('essai is', essai);
+
+
+  console.log(new Date(2022, 5, 25));
+  console.log('', date);
+  // console.log('', (2022, 5, 25).toDateString());
+
+  useEffect(() => {
+
+    const fetchReservations = async () => {
+      await fetch(`/api/item/${itemById}/reservations`)
+      .then((res) => res.json())
+      .then((data) => {
+        setResa(data.data)
+        let reservations = data.data
+        let datesReservations = []
+        // let filterResa = reservations.filter(e => datesReservations.push(new Date(e.date)))
+        let filterResa = reservations.filter(e => datesReservations.push(e.date))
+        console.log('datesReservations is', datesReservations);
+        // let filterResa = reservations.filter(e => datesReservations.push(e.date.toString()))
+        let newDatesFormat = []
+        // for (let i = 0; i<= datesReservations.length; i++) {
+        //   console.log('',datesReservations[i]);
+        // //  let newDates = datesReservations[i].forEach((e) => e = new Date(e))
+        // console.log(datesReservations[i].map(e => new Date(e)))
+        // //  console.log('', newDates);
+        // }
+        // console.log('', datesReservations);
+
+
+
+
+        // let newDatesFormat = []
+        // datesReservations.filter(e => newDatesFormat.push(e[0], e[1]))
+
+        // datesReservations.filter(e => newDatesFormat.push(new Date(e[0])))
+        console.log('newDatesFormat', newDatesFormat);
+
+
+
+        // console.log('filterResa', filterResa);
+        // setDisabledRanges(datesReservations)
+        // setEssai(datesReservations)
+
+
+        // console.log('filterResa', filterResa);
+
+        // console.log('reservations IS', reservations);
+
+      })
+    }
+  
+   fetchReservations()
+  }, [itemById]);
+
+  
+
+
+
 
   function tileDisabled({ date, view }) {
     // Add class to tiles in month view only
@@ -37,19 +112,22 @@ const CalendarReservation = ({ owner, item }) => {
   }
 
   console.log("date", date);
-  console.log("owner", owner);
-  console.log("item", item);
+  // console.log("owner", owner);
+  // console.log("item", item);
 
-  console.log('ownerId', owner._id);
-  console.log('itemById', itemById);
+  // console.log('ownerId', owner._id);
+  // console.log('itemById', itemById);
+
+
 
 
   const handleBooking = async (e) => {
     e.preventDefault();
+    if (!user ) console.log('pas possible de réserver');
 
-    // useEffect(() => {
 
-   
+
+
     await fetch(`/api/item/${itemById}`, {
       method: "POST",
       headers: {
@@ -69,7 +147,7 @@ const CalendarReservation = ({ owner, item }) => {
         console.error("failed to book the item", error);
       });
   };
-// }, [])
+
 
   return (
     <div>
@@ -82,15 +160,29 @@ const CalendarReservation = ({ owner, item }) => {
           tileDisabled={tileDisabled}
           minDate={new Date()}
         />
-        <Button>Book</Button>
+        {!user ? (
+          <Link to="/login">
+          <Button>Sign in to book!</Button>
+          </Link>
+        ) : (
+          <Button>Book</Button>
+
+        )
+        
+         }
       </Form>
     </div>
   );
 };
 
-const Form = styled.form``;
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center
+`;
 
 const Button = styled.button`
+
   margin-top: 20px;
   width: 250px;
   height: 40px;
