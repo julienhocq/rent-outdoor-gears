4b4542c91ecb97fdc7c182c3f665fcb9ea306ba1
julienhocq <julienhocq@gmail.com>
2022-06-13 16:59:22 -0400

    add dates reservation (case 1 day + range of days)

--- client/src/components/ConfirmationBooking.js
+++ client/src/components/ConfirmationBooking.js
@@ -1,15 +1,31 @@
 import styled from "styled-components";
-
+import moment from "moment";
 
 const ConfirmationBooking = () => {
+  const getNewBooking = JSON.parse(sessionStorage.getItem("NewBooking"));
 
-const getNewBooking = JSON.parse(sessionStorage.getItem("NewBooking"))
-console.log('getNewBooking', getNewBooking);
+  const dateReservation = getNewBooking.date;
 
+  let dateStart = new Date(getNewBooking.date[0]);
+  let dateEnd = new Date(getNewBooking.date[1]);
 
-    return (
-        <div>Yop</div>
-    )
-}
+  return (
+    <>
+      <p>Your item has been reserved!</p>
+      <p>Date(s):</p>
 
-export default ConfirmationBooking
+      {dateReservation.length === 1 ? (
+        <p>{moment.utc(dateStart).format("dddd Do YYYY")} </p>
+      ) : (
+        <>
+          <p>Start: {moment.utc(dateStart).format("dddd Do YYYY")} </p>
+          <p>End: {moment.utc(dateEnd).format("dddd Do YYYY")} </p>
+        </>
+      )}
+
+      <div></div>
+    </>
+  );
+};
+
+export default ConfirmationBooking;
